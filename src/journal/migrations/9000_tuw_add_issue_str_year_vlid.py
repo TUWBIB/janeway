5383810e34432ba625bd8fe445d8f93b0c0aa73b
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2019-11-06 11:21:13 +0100

    migrations

--- src/journal/migrations/9000_tuw_add_issue_str_year_vlid.py
+++ src/journal/migrations/9000_tuw_add_issue_str_year_vlid.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('journal', '0034_migrate_issue_types'),
+        ('journal', '0036_set_default_xslt'),
     ]
 
     operations = [
