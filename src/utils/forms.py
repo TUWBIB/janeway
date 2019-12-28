44cfcc4eca87f60117084ffa082e11bbf1220197
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2019-12-28 08:27:20 +0100

    adding keywords_de to KeywordModelForm somehow broke the journal settings page; hack around this for now

--- src/utils/forms.py
+++ src/utils/forms.py
@@ -38,40 +38,45 @@ class KeywordModelForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        current_keywords = self.instance.keywords.values_list("word", flat=True)
-        field = self.fields["keywords"]
-        field.initial = ",".join(current_keywords)
 
-        current_keywords_de = self.instance.keywords_de.values_list("word", flat=True)
-        field = self.fields["keywords_de"]
-        field.initial = ",".join(current_keywords_de)
+        if hasattr(self.instance, 'keywords'):
+            current_keywords = self.instance.keywords.values_list("word", flat=True)
+            field = self.fields["keywords"]
+            field.initial = ",".join(current_keywords)
+
+        if hasattr(self.instance, 'keywords_de'):
+            current_keywords_de = self.instance.keywords_de.values_list("word", flat=True)
+            field = self.fields["keywords_de"]
+            field.initial = ",".join(current_keywords_de)
 
 
     def save(self, commit=True, *args, **kwargs):
         instance = super().save(commit=commit, *args, **kwargs)
 
-        posted_keywords = self.cleaned_data.get('keywords', '').split(',')
-        for keyword in posted_keywords:
-            if keyword != '':
-                obj, _ = submission_models.Keyword.objects.get_or_create(
-                        word=keyword)
-                instance.keywords.add(obj)
+        if 'keywords' in self.cleaned_data:
+            posted_keywords = self.cleaned_data.get('keywords', '').split(',')
+            for keyword in posted_keywords:
+                if keyword != '':
+                    obj, _ = submission_models.Keyword.objects.get_or_create(
+                            word=keyword)
+                    instance.keywords.add(obj)
 
-        for keyword in instance.keywords.all():
-            if keyword.word not in posted_keywords:
-                instance.keywords.remove(keyword)
+            for keyword in instance.keywords.all():
+                if keyword.word not in posted_keywords:
+                    instance.keywords.remove(keyword)
 
 
-        posted_keywords_de = self.cleaned_data.get('keywords_de', '').split(',')
-        for keyword_de in posted_keywords:
-            if keyword_de != '':
-                obj, _ = submission_models.KeywordDe.objects.get_or_create(
-                        word=keyword_de)
-                instance.keywords_de.add(obj)
+        if 'keywords_de' in self.cleaned_data:
+            posted_keywords_de = self.cleaned_data.get('keywords_de', '').split(',')
+            for keyword_de in posted_keywords:
+                if keyword_de != '':
+                    obj, _ = submission_models.KeywordDe.objects.get_or_create(
+                            word=keyword_de)
+                    instance.keywords_de.add(obj)
 
-        for keyword_de in instance.keywords_de.all():
-            if keyword_de.word not in posted_keywords_de:
-                instance.keywords_de.remove(keyword_de)
+            for keyword_de in instance.keywords_de.all():
+                if keyword_de.word not in posted_keywords_de:
+                    instance.keywords_de.remove(keyword_de)
 
         if commit:
             instance.save()
