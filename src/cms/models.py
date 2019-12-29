cefb6bb8ccecf21189ddbeb11dbf90607925c16c
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2019-12-29 10:36:39 +0100

    lang specific nav items

--- src/cms/models.py
+++ src/cms/models.py
@@ -9,8 +9,15 @@ from django.db.models import Q
 from django.utils import timezone
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import ugettext_lazy as _
 
 from utils.logic import build_url_for_request
+
+LANGUAGE_CHOICES = (
+    (u'en', _('English')),
+    (u'de', _('German')),
+)
+
 
 class Page(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='page_content', null=True)
@@ -18,9 +25,9 @@ class Page(models.Model):
     object = GenericForeignKey('content_type', 'object_id')
 
     name = models.CharField(max_length=300, help_text="Page name displayed in the URL bar eg. about or contact")
-    display_name = models.CharField(max_length=100, help_text='Name of the page, max 100 chars, displayed '
+    display_name = models.CharField(max_length=100, help_text=_('Name of the page, max 100 chars, displayed '
                                                               'in the nav and on the header of the page eg. '
-                                                              'About or Contact')
+                                                              'About or Contact'))
     content = models.TextField(null=True, blank=True)
     is_markdown = models.BooleanField(default=True)
     edited = models.DateTimeField(auto_now=timezone.now)
@@ -41,6 +48,8 @@ class NavigationItem(models.Model):
     page = models.ForeignKey(Page, blank=True, null=True)
     has_sub_nav = models.BooleanField(default=False, verbose_name="Has Sub Navigation")
     top_level_nav = models.ForeignKey("self", blank=True, null=True, verbose_name="Top Level Nav Item")
+    language = models.CharField(max_length=200, blank=True, null=True, choices=LANGUAGE_CHOICES,
+        help_text=_('Language for which this nav item is displayed, leave empty if item is to be shown regardless of language'))
 
     def __str__(self):
         return self.link_name
