fef57664435cb2e046bd8c427fd5e843a4fae3f7
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2020-01-02 15:56:40 +0100

    use templatetag to filter sub nav items prepare theme for switch to cms page instead of hardcoded urls

---
+++ src/cms/templatetags/subnav.py
@@ -0,0 +1,13 @@
+from django.db.models import Q
+from django import template
+from cms import models as cms_models
+
+register = template.Library()
+
+
+@register.simple_tag
+def tag_get_sub_nav_items(top_nav_item,language):
+    return cms_models.NavigationItem.objects.language().fallbacks('en') \
+        .filter(top_level_nav=top_nav_item) \
+        .filter(Q(language=None) | Q(language=language)) \
+        .order_by('sequence')
