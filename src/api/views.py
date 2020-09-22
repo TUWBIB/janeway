b41dc2460ae9e163881d8150d38a693ba1811084
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2020-09-22 20:11:54 +0200

    OAI wip

--- src/api/views.py
+++ src/api/views.py
@@ -1,4 +1,6 @@
 import json
+import datetime
+import pytz
 
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -112,56 +114,178 @@ class ArticleViewSet(viewsets.ModelViewSet):
 
 @csrf_exempt
 def oai(request):
+    context = {}
+    context['journal'] = request.journal
+    context['responseDate'] = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
+    context['rv_value'] = request.scheme+'://'+request.META['HTTP_HOST']+request.path
+
+    params = {}
+
     if request.method == 'GET' or request.method == 'POST':
+        if request.method == 'GET': 
+            for k,v in request.GET.items(): params[k] = v
+        if request.method == 'POST': 
+            for k,v in request.POST.items(): params[k] = v
+        
+        l = []
+        for k,v in params.items():
+            l.append(k + '="' + v + '"')
+        context['rv_attribs'] = " ".join(l)
+    
 
         verb = None
-        if request.method == 'GET' and 'verb' in request.GET.keys():
-            verb = request.GET['verb'].lower()
+        if 'verb' in params.keys():
+            verb = params['verb']
 
-        if request.method == 'POST':
-            verb = request.POST.get('verb').lower()
 
-        if not verb or verb == 'listrecords':
-            articles = submission_models.Article.objects.filter(stage=submission_models.STAGE_PUBLISHED)
+        if verb is None or verb not in ('Identify', 'ListRecords', 'GetRecord', 'ListIdentifiers', 'ListMetadataFormats', 'ListSets'):
+            template = 'apis/OAI_Error.xml'
+            context['err_code'] = 'badVerb'
+            context['err_val'] = context['err_code']
+
+        elif verb == 'ListSets':
+            template = 'apis/OAI_Error.xml'
+            context['err_code'] = 'noSetHierarchy'
+            context['err_val'] = context['err_code']
+
+
+        elif verb == 'ListRecords':
+            articles = submission_models.Article.objects.filter(
+                    journal=request.journal,
+                    stage=submission_models.STAGE_PUBLISHED,
+                    identifier__id_type="doi",
+                )
             template = 'apis/OAI_ListRecords.xml'
-            context = {
-                'articles': articles,
-            }
-            return render(request, template, context, content_type="application/xml")
+            context['articles'] =  articles
 
-        if not verb or verb == 'listidentifiers':
-            articles = submission_models.Article.objects.filter(stage=submission_models.STAGE_PUBLISHED)
+        elif verb == 'ListIdentifiers':
+            a = set(params.keys())
+            b = set(['verb', 'from', 'until', 'metadataPrefix', 'set', 'resumptionToken'])
+            
+            if not a.issubset(b):
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'badArgument'
+                context['err_val'] = context['err_code']
+                return render(request, template, context, content_type="text/xml")
+
+            from_date = params['from'] if 'from' in params else None
+            until_date = params['until'] if 'until' in params else None
+            metadataprefix = params['metadataPrefix'] if 'metadataPrefix' in params else None
+            set_name = params['set'] if 'set' in params else None
+            resumption = params['resumptionToken'] if 'resumptionToken' in params else None
+
+            if set_name or set_name == '':
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'noSetHierarchy'
+                context['err_val'] = context['err_code']
+                return render(request, template, context, content_type="text/xml")
+
+            if resumption or resumption == '':
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'badResumptionToken'
+                context['err_val'] = context['err_code']
+                return render(request, template, context, content_type="text/xml")
+
+            articles = submission_models.Article.objects.filter(
+                    journal=request.journal,
+                    stage=submission_models.STAGE_PUBLISHED,
+                    identifier__id_type="doi",
+                )
             template = 'apis/OAI_ListIdentifiers.xml'
-            context = {
-                'articles': articles,
-            }
-            return render(request, template, context, content_type="application/xml")
+            context['articles'] =  articles
 
+        elif verb == 'ListMetadataFormats':
+            a = set(params.keys())
+            b = set(['verb', 'identifier'])
+            
+            if not a.issubset(b):
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'badArgument'
+                context['err_val'] = context['err_code']
+                return render(request, template, context, content_type="text/xml")
 
-        elif verb == 'listmetadataformats':
+            identifier = params['identifier'] if 'identifier' in params else None
+            parts = identifier.split(':')
+            if len(parts) != 3 or parts[0] != 'oai' or parts[1] != request.journal.domain:
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'idDoesNotExist'
+                context['err_val'] = context['err_code']
+                return render(request, template, context, content_type="text/xml")
+
+            doi = parts[3]
+            try:
+                article = submission_models.Article.objects.get(
+                        journal=request.journal,
+                        stage=submission_models.STAGE_PUBLISHED,
+                        identifier__id_type="doi",
+                        identifier__identifier=doi,
+                    )
+            except:
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'idDoesNotExist'
+                context['err_val'] = context['err_code']
+                return render(request, template, context, content_type="text/xml")
+            
             template = 'apis/OAI_ListMetadataFormats.xml'
-            context = {
-                'rv_value': 'rv_value_dummy',
-                'rv_identifier': 'rv_identifier_dummy',
-            }
-            return render(request, template, context, content_type="application/xml")
+     
+        elif verb == 'GetRecord':
+            a = set(params.keys())
+            b = set(['verb', 'identifier', 'metadataPrefix'])
+            
+            if not a.issubset(b):
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'badArgument'
+                context['err_val'] = context['err_code']
+                return render(request, template, context, content_type="text/xml")
 
-        elif verb == 'getrecord':
+            identifier = params['identifier'] if 'identifier' in params else None
+            metadataprefix = params['metadataPrefix'] if 'metadataPrefix' in params else None
+
+            if metadataprefix is None or identifier is None:
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'badArgument'
+                context['err_val'] = 'missing arguments, identifier and metadataPrefix required'
+                return render(request, template, context, content_type="text/xml")
+            
+            if metadataprefix != 'oai_dc':
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'cannotDisseminateFormat'
+                context['err_val'] = 'metadataPrefix must be oai_dc'
+                return render(request, template, context, content_type="text/xml")                
+
+
+            parts = identifier.split(':')
+            print (parts)
+            if len(parts) != 3 or parts[0] != 'oai' or parts[2] != request.journal.domain:
+                print ("cp1")
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'idDoesNotExist'
+                context['err_val'] = context['err_code']
+                return render(request, template, context, content_type="text/xml")
+
+            doi = parts[2]
+            try:
+                article = submission_models.Article.objects.get(
+                        journal=request.journal,
+                        stage=submission_models.STAGE_PUBLISHED,
+                        identifier__id_type="doi",
+                        identifier__identifier=doi,
+                    )
+            except Exception as e:
+                print (e)
+                template = 'apis/OAI_Error.xml'
+                context['err_code'] = 'idDoesNotExist'
+                context['err_val'] = context['err_code']
+                return render(request, template, context, content_type="text/xml")
+            
             template = 'apis/OAI_GetRecord.xml'
-            context = {
-                'rv_value': 'rv_value_dummy',
-                'rv_identifier': 'rv_identifier_dummy',
-            }
-            return render(request, template, context, content_type="application/xml")
+            context['article'] = article            
 
-
-        elif verb == 'identify':
+        elif verb == 'Identify':
             template = 'apis/OAI_Identify.xml'
-            context = {
-                'rv_value': request.scheme+'://'+request.META['HTTP_HOST']+request.path,
-                'repositoryName': request.journal.description,
-                'baseURL': request.scheme+'://'+request.META['HTTP_HOST']+request.path,
-                'adminEmail': 'repositum@tuwien.ac.at',
-                'deletedRecord': 'transient',
-            }
-            return render(request, template, context, content_type="application/xml")
+            context['repositoryName'] = request.journal.description
+            context['baseURL'] = request.scheme+'://'+request.META['HTTP_HOST']+request.path
+            context['adminEmail'] = 'repositum@tuwien.ac.at'
+            context['deletedRecord'] = 'no'
+
+        return render(request, template, context, content_type="text/xml")
