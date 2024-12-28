79bf11a05ccb1d9e1540e0c84e06d9810567c240
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2024-12-28 22:37:58 +0100

    cleanup... wip

--- src/utils/management/commands/zzz_cleanup_titles.py
+++ src/utils/management/commands/zzz_cleanup_titles.py
@@ -1,9 +1,13 @@
 
 from django.core.management.base import BaseCommand
+from django.utils.translation import activate
 from django.utils import translation
 from django.conf import settings
+from django.db import connection, transaction
 
 from submission import models as submission_models
+
+import xlsxwriter
 
 class Command(BaseCommand):
     """A management command to add missing default settings to journals."""
@@ -17,6 +21,8 @@ class Command(BaseCommand):
         :return: None
         """
         parser.add_argument('--dryrun', default=True)
+        parser.add_argument('--fileout', default='result.xlsx')
+        parser.add_argument('--pk', default=None)
 
     def handle(self, *args, **options):
         """Checks existing journal settings, adds missing ones.
@@ -25,51 +31,162 @@ class Command(BaseCommand):
         :param options: None
         :return: None
         """
-        WARN_TITLE_TUW_SET = 'title tuw set'
-        WARN_ARTICLE_LANGUAGE_ENGLISH_TITLE_EN_NOT_SET = 'english article, but english title not set'
-        WARN_ARTICLE_LANGUAGE_GERMAN_TITLE_DE_NOT_SET = 'german article, but german title not set'
-
         dryrun = options.get('dryrun', True)
+        fileout = options.get('fileout', 'result.xlsx')
+        pk = options.get('pk', None)
 
-        articles = submission_models.Article.objects.all().order_by('id')
+        wb = xlsxwriter.Workbook(fileout)
+        sheet = wb.add_worksheet('titles')
+        sheet.freeze_panes(1, 0)
+
+        row = 0
+        col = iter(range(0,30))
+        sheet.write(row,next(col),'id')
+        sheet.write(row,next(col),'language')
+        sheet.write(row,next(col),'title')
+        sheet.write(row,next(col),'title_en')
+        sheet.write(row,next(col),'title_de')
+        sheet.write(row,next(col),'title_de_tuw')
+        sheet.write(row,next(col),'subtitle')
+        sheet.write(row,next(col),'subtitle_en')
+        sheet.write(row,next(col),'subtitle_de')
+        sheet.write(row,next(col),'subtitle_de_tuw')
+        sheet.write(row,next(col),'abstract')
+        sheet.write(row,next(col),'abstract_en')
+        sheet.write(row,next(col),'abstract_de')
+        sheet.write(row,next(col),'abstract_de_tuw')
+        sheet.write(row,next(col),'action')
+
+        if pk is None:
+            articles = submission_models.Article.objects.all().order_by('id')
+        else:
+            articles = submission_models.Article.objects.filter(pk=pk).order_by('id')
+
         count = 0
         for article in articles:
             l = []
-            warnings = []
-#            l.append(str(article.journal.id))
-            title = article.__dict__.get('title','')
-            title_en = article.__dict__.get('title_en','')
-            title_de = article.__dict__.get('title_de','')
+            title = article.getTitleRAW
+            title_en = article.getTitleEN
+            title_de = article.getTitleDE
             title_de_tuw = article.title_de_tuw if article.title_de_tuw else ''
+            subtitle = article.getSubTitleRAW
+            subtitle_en = article.getSubTitleEN
+            subtitle_de = article.getSubTitleDE
+            subtitle_de_tuw = article.subtitle_de_tuw if article.subtitle_de_tuw else ''
+            abstract = article.getAbstractRAW
+            abstract_en = article.getAbstractEN
+            abstract_de = article.getAbstractDE
+            abstract_de_tuw = article.abstract_de_tuw if article.abstract_de_tuw else ''
+
             language = article.language
-            if title is None: title = ''
-            if title_en is None: title_en = ''
-            if title_de is None: title_de = ''
             if language is None: language = ''
 
-            l.append(str(article.pk))
-            l.append(language)                        
-            l.append(f'title = {title}')
-            l.append(f'title_en = {title_en}')
-            l.append(f'title_de = {title_de}')
-            l.append(f'title_de_tuw = {title_de_tuw}')
+            ## title corrections
 
-            s = ';'.join(l)
-            if language == 'deu' and not title_de:
-                warnings.append(WARN_ARTICLE_LANGUAGE_GERMAN_TITLE_DE_NOT_SET)
-            if language == 'eng' and not title_en:
-                warnings.append(WARN_ARTICLE_LANGUAGE_ENGLISH_TITLE_EN_NOT_SET)
-            if title_de_tuw:
-                warnings.append(WARN_TITLE_TUW_SET)
+            # no parallel title
+            # article language german
+            # english title set
+            # >> delete english title
+            if language == 'deu' and title and title_en and title_de and not title_de_tuw:
+                l.append('delete english title')
+                article.__dict__['title_en'] = None
+                if not dryrun:
+                    article.save()
 
-            if warnings:
-                count += 1
-                print(s)            
-                print(warnings)
-                
-        print(f'{count} articles with warnings')
+            # no parallel title
+            # article language english
+            # german title set
+            # >> delete german title
+            if language == 'eng' and title and title_en and title_de and not title_de_tuw:
+                l.append('delete german title')
+                article.__dict__['title_de'] = None
+                if not dryrun:
+                    article.save()
 
+            # parallel title set
+            # article language germen
+            # >> delete parallel title, set title to german title, set english title to parallel title
+            if language == 'deu' and title and title_en and title_de and title_de_tuw:
+                l.append('set german title as main title, set english title to parallel title, delete parallel title')
+                article.title_de_tuw = None
+                with translation.override('en'):
+                    article.title = title_de_tuw 
+                with translation.override('de'):
+                    article.title = title_de
+                if not dryrun:
+                    article.save()
 
+            row += 1 
+            col = iter(range(0,30))
+            sheet.write(row,next(col),article.pk)
+            sheet.write(row,next(col),language)
+            sheet.write(row,next(col),title)
+            sheet.write(row,next(col),title_en)
+            sheet.write(row,next(col),title_de)
+            sheet.write(row,next(col),title_de_tuw)
+            sheet.write(row,next(col),subtitle)
+            sheet.write(row,next(col),subtitle_en)
+            sheet.write(row,next(col),subtitle_de)
+            sheet.write(row,next(col),subtitle_de_tuw)
+            sheet.write(row,next(col),abstract)
+            sheet.write(row,next(col),abstract_en)
+            sheet.write(row,next(col),abstract_de)
+            sheet.write(row,next(col),abstract_de_tuw)
+            sheet.write(row,next(col),'; '.join(l))
 
+        sheet = wb.add_worksheet('titles after change')
+        sheet.freeze_panes(1, 0)
 
+        row = 0
+        col = iter(range(0,30))
+        sheet.write(row,next(col),'id')
+        sheet.write(row,next(col),'language')
+        sheet.write(row,next(col),'title')
+        sheet.write(row,next(col),'title_en')
+        sheet.write(row,next(col),'title_de')
+        sheet.write(row,next(col),'title_de_tuw')
+        sheet.write(row,next(col),'subtitle')
+        sheet.write(row,next(col),'subtitle_en')
+        sheet.write(row,next(col),'subtitle_de')
+        sheet.write(row,next(col),'subtitle_de_tuw')
+        sheet.write(row,next(col),'abstract')
+        sheet.write(row,next(col),'abstract_en')
+        sheet.write(row,next(col),'abstract_de')
+        sheet.write(row,next(col),'abstract_de_tuw')
+
+        for article in articles:
+            title = article.getTitleRAW
+            title_en = article.getTitleEN
+            title_de = article.getTitleDE
+            title_de_tuw = article.title_de_tuw if article.title_de_tuw else ''
+            subtitle = article.getSubTitleRAW
+            subtitle_en = article.getSubTitleEN
+            subtitle_de = article.getSubTitleDE
+            subtitle_de_tuw = article.subtitle_de_tuw if article.subtitle_de_tuw else ''
+            abstract = article.getAbstractRAW
+            abstract_en = article.getAbstractEN
+            abstract_de = article.getAbstractDE
+            abstract_de_tuw = article.abstract_de_tuw if article.abstract_de_tuw else ''
+
+            language = article.language
+            if language is None: language = ''
+
+            row += 1 
+            col = iter(range(0,30))
+            sheet.write(row,next(col),article.pk)
+            sheet.write(row,next(col),language)
+            sheet.write(row,next(col),title)
+            sheet.write(row,next(col),title_en)
+            sheet.write(row,next(col),title_de)
+            sheet.write(row,next(col),title_de_tuw)
+            sheet.write(row,next(col),subtitle)
+            sheet.write(row,next(col),subtitle_en)
+            sheet.write(row,next(col),subtitle_de)
+            sheet.write(row,next(col),subtitle_de_tuw)
+            sheet.write(row,next(col),abstract)
+            sheet.write(row,next(col),abstract_en)
+            sheet.write(row,next(col),abstract_de)
+            sheet.write(row,next(col),abstract_de_tuw)
+
+        wb.close()
     
