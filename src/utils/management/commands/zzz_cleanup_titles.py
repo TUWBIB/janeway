5c4e3f8cb782a13ef214591a85081548a10647fb
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2025-04-11 17:28:33 +0200

    cleanup titles..wip

--- src/utils/management/commands/zzz_cleanup_titles.py
+++ src/utils/management/commands/zzz_cleanup_titles.py
@@ -23,8 +23,8 @@ class Command(BaseCommand):
         parser.add_argument('--dryrun', default=True)
         parser.add_argument('--fileout', default='result.xlsx')
         parser.add_argument('--mode', default='tsa')
-        parser.add_argument('--minpk', default='tsa')
-        parser.add_argument('--maxpk', default='tsa')
+        parser.add_argument('--minpk', default=1)
+        parser.add_argument('--maxpk', default=99999)
 
     def handle(self, *args, **options):
         """Checks existing journal settings, adds missing ones.
@@ -108,8 +108,25 @@ class Command(BaseCommand):
 
         articles = submission_models.Article.objects.filter(pk__gte=minpk,pk__lte=maxpk).order_by('id')
 
+        l_title_is_english_delete_german =  [
+            #jfm
+            1,113,120,126,133,139,613,
+            #oes
+            185,204,419,434,435,436,444,446,447,458,460,461,472
+        ]
+
         l_title_is_german_delete_english =  [
-            1,2,3
+            # jfm
+            2,3,10,11,12,19,20,21,27,28,29,36,37,38,43,44,45,50,51,52,57,58,59,64,65,66,71,72,73,79,80,81,86,87,88,93,94,95,100,101,102,107,108,109,114,115,
+            121,122,127,128,134,135,140,141,
+            # oes
+            145,146,147,155,156,161,162,167,168,169,171,172,173,178,179,186,187,188,194,195,196,197,
+            205,206,211,212,213,214,215,217,218,219,224,225,226,231,232,233,234,235,237,238,239,243,
+            244,245,246,249,250,251,252,253,256,257,258,263,264,265,270,271,272,273,274,279,280,281,
+            286,287,288,289,295,296,297,298,303,304,305,310,318,319,320,321,322,326,327,328,329,330,
+            334,335,336,337,346,347,348,349,350,355,356,357,358,362,363,364,365,369,370,371,379,380,
+            381,382,383,384,389,390,391,392,403,404,405,406,420,421,422,429,432,433,445,457,459,473,
+            474,475,486,487,488,489,490,506,507,508,509,510,520,521,522,523,539
         ]
 
         for article in articles:
@@ -159,40 +176,83 @@ class Command(BaseCommand):
 
             ## title corrections
             if 't' in mode:
+                # hardcoded cases
                 if not language:
                     if article.pk in l_title_is_german_delete_english and title_en:
+                        l.append('set article language german')               
+                        article.language = 'deu'
+                        if not dryrun:
+                            print(f"set lang 'deu' for {article.pk}")
+                            article.save()
                         l.append('delete english title')                        
                         article.__dict__['title_en'] = None
                         title_changed = True
-                
+
+                    elif article.pk in l_title_is_english_delete_german and title_de:
+                        l.append('set article language english')                        
+                        article.language = 'eng'
+                        if not dryrun:
+                            print(f"set lang 'eng' for {article.pk}")
+                            article.save()
+                        l.append('delete german title')                        
+                        article.__dict__['title_de'] = None
+                        title_changed = True
+
+
                 # no parallel title
                 # article language german
                 # english title set
-                # >> delete english title
-                elif language == 'deu' and title and title_en and title_de and not title_de_tuw:
-                    l.append('delete english title')
+                # german title set
+                # >> move title from english to german
+                elif language == 'deu' and title and title_en and not title_de and not title_de_tuw:
+                    l.append('move title from english to german')
+                    article.__dict__['title_de'] = title_en
                     article.__dict__['title_en'] = None
                     title_changed = True
 
                 # no parallel title
                 # article language english
+                # english title not set
                 # german title set
+                # >> move title from german to english
+                elif language == 'eng' and title and title_de and not title_en and not title_de_tuw:
+                    l.append('move title from german to english')
+                    article.__dict__['title_en'] = title_de
+                    article.__dict__['title_de'] = None
+                    title_changed = True
+
+
+                # no parallel title
+                # article language german
+                # english title set
+                # german title set
+                # german and english title equal
+                # >> delete english title
+                elif language == 'deu' and title and title_en and title_de and not title_de_tuw and (title_en.strip() == title_de.strip()):
+                    l.append('delete english title')
+                    article.__dict__['title_en'] = None
+                    title_changed = True
+
+
+                # no parallel title
+                # article language english
+                # english title set
+                # german title set
+                # german and english title equal
                 # >> delete german title
-                elif language == 'eng' and title and title_en and title_de and not title_de_tuw:
+                elif language == 'eng' and title and title_en and title_de and not title_de_tuw and (title_en.strip() == title_de.strip()):
                     l.append('delete german title')
                     article.__dict__['title_de'] = None
                     title_changed = True
 
                 # parallel title set
                 # article language germen
-                # >> delete parallel title, set title to german title, set english title to parallel title
+                # >> delete parallel title, set title to german title, set parallel_title as english 
                 elif language == 'deu' and title and title_en and title_de and title_de_tuw:
-                    l.append('set german title as main title, set english title to parallel title, delete parallel title')
+                    l.append('set german title as main title, set parallel title as english title, delete parallel title')
+                    title = title_de
+                    article.__dict__['title_en'] = title_de_tuw
                     article.title_de_tuw = None
-                    with translation.override('en'):
-                        article.title = title_de_tuw 
-                    with translation.override('de'):
-                        article.title = title_de
                     title_changed = True
 
             ### subtitle corrections
