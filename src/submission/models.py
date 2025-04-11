9db00e2e0ac880010cc71f838f73415b9b7c0ec0
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2025-04-11 16:44:19 +0200

    mark title as non required

--- src/submission/models.py
+++ src/submission/models.py
@@ -634,6 +634,8 @@ class Article(AbstractLastModifiedModel):
     )
     title = JanewayBleachCharField(
         max_length=999,
+        blank=True,
+        null=True,
         help_text=_('Your article title'),
     )
     subtitle = JanewayBleachCharField(
