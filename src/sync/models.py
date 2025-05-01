ca3505ac3c36b91c4847ac39d4a974f0c3c49b5b
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2025-05-01 12:15:12 +0200

    need to store datacite and alma data related to issues somewhere; lets keep it separated from core code

--- src/sync/models.py
+++ src/sync/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+DATACITE_STATUS_NONE=''
+DATACITE_STATUS_DRAFT='draft'
+DATACITE_STATUS_FINDABLE='findable'
+DATACITE_STATUS_REGISTERED='registered'
+
+
+DATACITE_STATUS_CHOICES = [
+    (DATACITE_STATUS_NONE, DATACITE_STATUS_NONE),
+    (DATACITE_STATUS_NONE, DATACITE_STATUS_DRAFT),
+    (DATACITE_STATUS_FINDABLE, DATACITE_STATUS_FINDABLE),
+    (DATACITE_STATUS_REGISTERED, DATACITE_STATUS_REGISTERED),
+]
+class DataCite(models.Model):
+
+    article = models.ForeignKey(
+        'submission.Article',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    issue = models.ForeignKey(
+        'journal.Issue',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    state = models.CharField(max_length=20, blank=True, null=True, choices=DATACITE_STATUS_CHOICES)
+    ts = models.DateTimeField(blank=True, null=True)
+
+class Alma(models.Model):
+    article = models.ForeignKey(
+        'submission.Article',
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    issue = models.ForeignKey(
+        'journal.Issue',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    mmsid = models.CharField(max_length=50, blank=True, null=True,)
+    ac = models.CharField(max_length=50, blank=True, null=True,)
+
+
