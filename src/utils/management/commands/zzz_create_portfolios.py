24e9c07689d54d2af5e02e3801a44875af42fc63
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2025-08-05 15:21:21 +0200

    complete bulk-creation of portfolios

--- src/utils/management/commands/zzz_create_portfolios.py
+++ src/utils/management/commands/zzz_create_portfolios.py
@@ -35,6 +35,7 @@ class Command(BaseCommand):
         """
         parser.add_argument('--journal_code', default=None)
         parser.add_argument('--max', default=None)
+        parser.add_argument('--offset', default=None)
         parser.add_argument('--mmsid', default=None)
 
     def handle(self, *args, **options):
@@ -52,10 +53,23 @@ class Command(BaseCommand):
             print("error in configuration")
             exit(1)
 
+        create_portfolios = alma_portfolios.get('create_portfolios',False)
+        if not create_portfolios:
+            print("creating portfolios disabled in config. exiting.")
+            exit(0)
+
+        collectionid = alma_portfolios.get('collection_id', None)
+        serviceid = alma_portfolios.get('service_id', None)
+
+        if not collectionid or not serviceid:
+            print("error. collectionid or serviceid not set in config")
+            exit(1)
 
         journal_code = options.get('journal_code', None)
         max_records = options.get('max', None)
         if max_records: max_records = int(max_records)
+        offset = options.get('offset',None)
+        offset = int(offset) if offset else 0
         mmsid_to_process = options.get('mmsid', None)
         if mmsid_to_process: mmsid_to_process = str(mmsid_to_process)
         if journal_code:
@@ -66,15 +80,23 @@ class Command(BaseCommand):
             articles = submission_models.Article.objects.all().order_by('journal__code','id')
 
         for i,article in enumerate(articles):
-            if max_records and i >= max_records:
+            if i < offset:
+                continue
+
+            if max_records and i >= (max_records + offset):
                 break
 
-            print(f"processing {article.journal.code} {article.id} {article.title}")
+            print(f"processing #{i} {article.journal.code} {article.id} {article.title}")
             mmsid = article.get_identifier('mmsid')
             if not mmsid:
                 print(f"\t skipping, no mmsid")
                 continue
-            print(f"\t mmsid: {mmsid}")
+            doi = article.get_identifier('doi')
+            if not doi:
+                print(f"\t skipping, no doi")
+                continue
+
+            print(f"\t mmsid: {doi}")
 
             if mmsid_to_process and mmsid_to_process.strip() != mmsid.strip():
                 print(f"\t skipping, mmsid not in parms")
@@ -93,6 +115,17 @@ class Command(BaseCommand):
                 cnt = int(match.group(1))
                 if cnt > 0:
                     print("\t skipping, portfolio already exists")
+                    continue
+
+            url = f"https://doi.org/{doi}"
+            result = api.createPortfolio(collectionid,serviceid,mmsid,url)
+            xml = result.data
+            errs = result.errs
+            if errs:
+                for err in errs:
+                    print(err)
+                exit(1)
+
 
         
 
