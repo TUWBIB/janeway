49723399dbf34066c17a6e6f80653f8fd8775595
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2025-09-04 14:34:24 +0200

    quality of life improvements for helper scripts

--- tuw_tools/drop_all_except_session_and_execute.sh
+++ tuw_tools/drop_all_except_session_and_execute.sh
@@ -3,3 +3,4 @@ sed -i '/django_session/d' drop_all_except_session.sql
 sed -i '/concat/d' drop_all_except_session.sql
 sed -i '1s/^/SET FOREIGN_KEY_CHECKS = 0;\n/' drop_all_except_session.sql
 echo 'SET FOREIGN_KEY_CHECKS = 1;' >> drop_all_except_session.sql
+mysql -u $1 -p$2 $3 < drop_all_except_session.sql
