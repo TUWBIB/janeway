61046a7fd291d608359c3519574d2828db123493
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2025-10-23 16:36:16 +0200

    domains for test system

---
+++ tuw_tools/test_domains.sh
@@ -0,0 +1,5 @@
+mysql -u $1 -p$2 $3 -e "UPDATE journal_journal SET domain='test.oes.tuwien.ac.at' WHERE code='OES';
+UPDATE journal_journal SET domain='test.journal.ifm.tuwien.ac.at' WHERE code='JFM';
+UPDATE journal_journal SET domain='test-arw-proceedings.acin.tuwien.ac.at' WHERE code='ARW';
+UPDATE journal_journal SET domain='test-iotw-proceedings.dsg.tuwien.ac.at' WHERE code='IOTW';
+UPDATE journal_journal SET domain='test-euroforth.complang.tuwien.ac.at' WHERE code='EF';"
