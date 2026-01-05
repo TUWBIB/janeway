8e31af872fbae9c6ac39410eb76109c0c871676b
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2026-01-05 13:56:52 +0100

    make self registration configurable cleanup of registration form actually return 404 if registration is disabled

--- src/core/views.py
+++ src/core/views.py
@@ -33,6 +33,7 @@ from django.utils.html import mark_safe
 from django.utils import translation
 from django.db.models import Q, OuterRef, Subquery, Count, Avg
 from django.views import generic
+from django.http import HttpResponseNotFound
 
 from core import models, forms, logic, workflow, files, models as core_models
 from core.model_utils import NotImplementedField, search_model_admin
@@ -314,6 +315,24 @@ def register(request):
     :param request: HttpRequest object
     :return: HttpResponse object
     """
+
+    try:
+        tuw_self_registration = setting_handler.get_setting(
+                'general',
+                'tuw_self_registration',
+                request.journal
+            ).value
+    except:
+        tuw_self_registration = 'both'
+
+    print("tuw_self_registration",tuw_self_registration)
+
+
+    if tuw_self_registration not in ['direct','orcid','both']:
+        return HttpResponseNotFound()
+
+    # 'no','direct','orcid','both'. If 
+
     context = {}
     initial = {}
     token, token_obj = request.GET.get('token', None), None
