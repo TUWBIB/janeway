f2c27af893680abd90918f706cc567c3c5a1c513
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2026-01-05 16:00:26 +0100

    add some filters

--- src/utils/management/commands/zzz_set_settings_from_file.py
+++ src/utils/management/commands/zzz_set_settings_from_file.py
@@ -24,6 +24,10 @@ class Command(BaseCommand):
         :return: None
         """
         parser.add_argument('--filename', required=True)
+        parser.add_argument('--ignore-settings', required=False)
+        parser.add_argument('--restrictto-settings', required=False)
+        parser.add_argument('--overwrite', required=False)
+        parser.add_argument('--journal-codes', required=False)
 
     def handle(self, *args, **options):
         """Synchronizes settings to journals.
@@ -33,7 +37,20 @@ class Command(BaseCommand):
         :return: None
         """
         translation.activate('en')
+
+        print(options)
+
         filename = options.get('filename',None)
+        ignore_settings = options.get('ignore_settings',None)
+        ignore_settings = [] if ignore_settings is None else ignore_settings.split(',') 
+        restrictto_settings = options.get('restrictto_settings',None)
+        restrictto_settings = [] if restrictto_settings is None else restrictto_settings.split(',') 
+        journal_codes = options.get('journal_codes',None)
+        journal_codes = [] if journal_codes is None else journal_codes.split(',') 
+        overwrite = options.get('overwrite',"True")
+        overwrite = "True" if overwrite is None else overwrite
+        overwrite = False if overwrite.lower() == 'false' else True
+
 
         with codecs.open(filename, 'r+', encoding='utf-8') as json_data:
             default_data = json.load(json_data, object_pairs_hook=OrderedDict)
@@ -42,6 +59,15 @@ class Command(BaseCommand):
 
                 for o in default_data:
                     setting_name = o['setting']['name']
+
+                    if setting_name in ignore_settings:
+                        print (f"setting ignored, {setting_name}")
+                        continue
+
+                    if restrictto_settings and setting_name not in restrictto_settings:
+                        print (f"setting ignored, {setting_name}")
+                        continue
+
                     setting_group_name = o['group']['name']
                     l_editable_by = o['editable_by']
 
@@ -52,6 +78,9 @@ class Command(BaseCommand):
 
                     d_value = o['value']
                     for journal_code,value in d_value.items():
+                        if journal_codes and journal_code not in journal_codes:
+                            print (f"setting ignored, {setting_name} due to journal {journal_code} not in parameter list")
+                            continue
 
                         journal = None if journal_code == 'default' else journal_models.Journal.objects.get(code=journal_code)
                         group,_ = core_models.SettingGroup.objects.get_or_create(name=setting_group_name)
@@ -64,11 +93,20 @@ class Command(BaseCommand):
                             for v in l_role:
                                 setting.editable_by.add(v)
                             setting.save()
-                        setting_value,_ = core_models.SettingValue.objects.get_or_create(setting=setting,journal=journal)
+                        setting_value,value_created = core_models.SettingValue.objects.get_or_create(setting=setting,journal=journal)
 
                         if not isinstance(value,str):
                             value = json.dumps(value,ensure_ascii=False)
 
-                        setting_value.value = value
-                        setting_value.save()
-                        print (f"setting saved, {setting_name} for {journal_code} with {value}")
+                        if value_created:
+                            setting_value.value = value
+                            setting_value.save()
+                            print (f"setting saved, {setting_name} for {journal_code} with {value}")                            
+                        else:
+                            if overwrite:
+                                setting_value.value = value
+                                setting_value.save()
+                                print (f"setting saved, {setting_name} for {journal_code} with {value}")                                                            
+                            else:
+                                print (f"overwrite false, setting not changed, {setting_name} for {journal_code} with {value},kept value {setting_value.value}")
+                        
