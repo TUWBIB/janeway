9dbd13f8a158fba376ec1c028bd21a2b59e99a14
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2026-02-09 16:08:42 +0100

    add two new proceedings sites

--- tuw_tools/dev_environment.sh
+++ tuw_tools/dev_environment.sh
@@ -3,4 +3,6 @@ UPDATE journal_journal SET domain='jfm.zzz' WHERE code='JFM';
 UPDATE journal_journal SET domain='arw.zzz' WHERE code='ARW';
 UPDATE journal_journal SET domain='iotw.zzz' WHERE code='IOTW';
 UPDATE journal_journal SET domain='ef.zzz' WHERE code='EF';
+UPDATE journal_journal SET domain='airov.zzz' WHERE code='AIROV';
+UPDATE journal_journal SET domain='isvcs.zzz' WHERE code='ISVCS';
 UPDATE core_contacts SET email='it@ub.tuwien.ac.at';"
