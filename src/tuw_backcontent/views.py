dcc628adf386080ae4f288b017964a263677c914
Oliver Lehner <oliver.lehner@tuwien.ac.at>
2026-09-25 13:37:48 +0200

    use current 1.8+ author forms, remove old ajax hacks

--- src/tuw_backcontent/views.py
+++ src/tuw_backcontent/views.py
@@ -36,8 +36,6 @@ from tuw_backcontent import forms as bc_forms
 logger = get_logger(__name__)
 
 
-# Create your views here.
-
 ## added TUW
 ## backcontent
 
@@ -85,13 +83,9 @@ def backcontent_article(request, article_id):
         pub_form = bc_forms.PublicationInfo(instance=article)
         remote_form = bc_forms.RemoteArticle(instance=article)
         galley_form = production_forms.GalleyForm()
-
-        modal = None
+        new_author_form = submission_forms.EditFrozenAuthor()
 
         if request.POST:
-            if 'add_author' in request.POST:
-                return handleAddAuthor(request, article)
-            
             if 'file' in request.FILES:
                 return handleFileUpload(request, article)
 
@@ -128,9 +122,10 @@ def backcontent_article(request, article_id):
         'pub_form': pub_form,
         'galleys': prod_logic.get_all_galleys(article),
         'remote_form': remote_form,
-        'modal': modal,
         'additional_fields': additional_fields,
-        'galley_form': galley_form
+        'galley_form': galley_form,
+        'authors': submission_logic.get_current_authors(article, request),
+        "new_author_form": new_author_form,
     }
 
     return render(request, template, context)
@@ -142,141 +137,33 @@ def backcontent_delete_article(request, article_id):
     messages.add_message(request, messages.SUCCESS, '%s deleted', article_id)
     return redirect(reverse('backcontent'))
 
+# TODO: what do to about last_changed_author``
 @editor_user_required
-def backcontent_order_authors(request, article_id):
-    article = get_object_or_404(submission_models.Article, pk=article_id)
-    author_pks = [int(pk) for pk in request.POST.getlist('authors[]')]
-    for author in article.authors.all():
-        order = author_pks.index(author.pk)
-        author_order, c = submission_models.ArticleAuthorOrder.objects.get_or_create(
-            article=article,
-            author=author,
-            defaults={'order': order}
-        )
-
-        if not c:
-            author_order.order = order
-            author_order.save()
-
-    article.snapshot_authors()
-
-    return HttpResponse('Complete')
-
-
-@editor_user_required
-def backcontent_delete_author(request, article_id, author_id):
-    """Allows submitting author to delete an author object."""
+def backcontent_add_author(request,article_id):
     article = get_object_or_404(
         submission_models.Article,
         pk=article_id,
-        journal=request.journal
+        journal=request.journal,
     )
-    author = get_object_or_404(
-        core_models.Account,
-        pk=author_id
+    new_author_form = submission_forms.EditFrozenAuthor()
+    last_changed_author = None
+
+    if request.method == "POST":
+        if "add_author" in request.POST:
+            last_changed_author = submission_logic.add_new_author_from_form(
+                request,
+                article,
+            )
+
+        elif "search_authors" in request.POST:
+            search_term = request.POST.get("author_search_text")
+            author = submission_logic.add_author_from_search(search_term, request, article)
+            last_changed_author = author
+    
+    return redirect(
+        reverse("backcontent_article", kwargs={"article_id": article_id})
     )
 
-    article.authors.remove(author)
-
-    if article.correspondence_author == author:
-        article.correspondence_author = None
-        article.save()
-
-    try:
-        ordering = submission_models.ArticleAuthorOrder.objects.get(
-            article=article,
-            author=author,
-        ).delete()
-    except submission_models.ArticleAuthorOrder.DoesNotExist:
-        pass
-
-    try:
-        ordering = submission_models.FrozenAuthor.objects.get(
-            article=article,
-            author=author,
-        ).delete()
-    except submission_models.FrozenAuthor.DoesNotExist:
-        print("frozen author does not exists")
-        pass
-       
-    return redirect(reverse('backcontent_article', kwargs={'article_id': article_id}))
-
-@editor_user_required
-def backcontent_add_author(request,article_id):
-    s=request.POST.get("search","")
-    message="unknown"
-    status="info"
-    author = None
-
-    # check email or orcid or gndid
-    authors=core_models.Account.objects.filter(email=s) | core_models.Account.objects.filter(orcid=s) | core_models.Account.objects.filter(gndid=s)
-    if authors:
-        if authors.count()>1:
-            message="multiple authors found, something's very fishy"
-            status="error"
-        else:
-            author=authors[0]
-    else:
-        l = s.split(' ')
-        if len(l)==1:
-            authors = core_models.Account.objects.filter(last_name__istartswith=l[0].lower())
-        else:
-            authors = core_models.Account.objects.filter(last_name__istartswith=l[0].lower()) & core_models.Account.objects.filter(first_name__istartswith=l[1].lower())
-
-        if authors:
-            if authors.count()>1:
-                message="sorry, multiple authors found"
-                status="error"
-            else:
-                author = authors[0]
-         
-
-    if author:
-        article=submission_models.Article.objects.filter(id=article_id)[0]
-
-        #check if account already has author role for this journal
-        role=core_models.Role.objects.filter(name='author')[0]
-        accountroles=core_models.AccountRole.objects.filter(user=author,journal=request.journal,role=role)
-        if accountroles:
-            pass
-        else:
-            author.add_account_role(role_slug='author', journal=request.journal)
-
-        #check if author already added to article
-        found=False
-
-        for a in article.authors.all():
-            if a.id==author.id:
-                found=True
-
-        if not found:
-            article.authors.add(author)
-            aao=setAuthorOrder(article,author)
-            submission_models.ArticleAuthorOrder.objects.get_or_create(article=article, author=author)
-            article.snapshot_authors()
-
-            message="author added"
-            status="success"
-            context = { 
-                'url': reverse('backcontent_delete_author', kwargs={'article_id': article.pk, 'author_id': author.pk }), 
-                'aao': serializers.serialize('json', [aao], fields=["pk","order"]),  
-                'author' : serializers.serialize('json',[aao.author], fields=["pk","last_name","first_name","email"]) 
-            }
-        else:
-            message="author already set"
-            status="warning"
-    else:
-        message="no author found for "+s
-        status="warning"
-
-    if status=="success": 
-        response = JsonResponse({'status': status,'message':message,
-                'context': json.dumps(context)})
-    else:
-        response= JsonResponse({'status': status,'message':message})
-
-
-    return response
 
 @editor_user_required
 def backcontent_preview_xml_galley(request, article_id, galley_id):
@@ -305,60 +192,6 @@ def backcontent_preview_xml_galley(request, article_id, galley_id):
 
     return render(request, template, context)
 
-
-def handleAddAuthor(request, article):
-    author_form = bc_forms.BackContentAuthorForm(request.POST)
-    modal = 'author'
-    context = { }
-
-
-    author_exists = submission_logic.check_author_exists(request.POST.get('email'))
-    if author_exists:
-        author_on_article = article.authors.filter(id=author_exists.id)
-
-        if not author_on_article:
-            article.authors.add(author_exists)
-            aao=setAuthorOrder(article,author_exists)
-            article.snapshot_authors()
-
-            messages.add_message(request, messages.SUCCESS, '%s added to the article' % author_exists.full_name())
-            context = { 
-                    'url': reverse('backcontent_delete_author', kwargs={'article_id': article.pk, 'author_id': author_exists.pk }), 
-                    'aao': serializers.serialize('json', [aao], fields=["pk","order"]),  
-                    'author' : serializers.serialize('json',[author_exists], fields=["pk","last_name","first_name","email"]) 
-            }
-        else:
-            messages.add_message(request, messages.ERROR, '%s is already author' % author_exists.full_name())
-    else:
-        if author_form.is_valid():
-            if author_form.cleaned_data["first_name"] and author_form.cleaned_data["last_name"]:
-                new_author = author_form.save(commit=False)
-                new_author.username = new_author.email
-                new_author.set_password(shared.generate_password())
-                new_author.save()
-                new_author.add_account_role(role_slug='author', journal=request.journal)
-
-                article.authors.add(new_author)
-                aao=setAuthorOrder(article,new_author)
-                article.snapshot_authors()
-                
-                messages.add_message(request, messages.SUCCESS, '%s added to the article' % new_author.full_name())
-                context = { 
-                    'url': reverse('backcontent_delete_author', kwargs={'article_id': article.pk, 'author_id': new_author.pk }), 
-                    'aao': serializers.serialize('json', [aao], fields=["pk","order"]),  
-                    'author' : serializers.serialize('json',[new_author], fields=["pk","last_name","first_name","email"]) 
-                }
-            else:
-                messages.add_message(request, messages.ERROR, '%s could not be found. Enter Firstname and Lastname' % request.POST.get('email'))
-        else:
-            messages.add_message(request, messages.ERROR,'% form invalid') 
-            print (author_form.errors)
-
-    data = {
-        'msg': loader.render_to_string('common/elements/messages.html', { 'messages': get_messages(request) }, None),
-        'context': json.dumps(context)
-    }
-    return JsonResponse(data)
 
 def handleFileUpload(request, article):
     from production import logic as production_logic, forms as production_forms
@@ -429,13 +262,6 @@ def handleSaveForm(request, article):
         if article_form.is_valid():
             article_form.save(request=request)
 
-        correspondence_author = request.POST.get('main-author', None)
-
-        if correspondence_author:
-            author = core_models.Account.objects.get(pk=correspondence_author)
-            article.correspondence_author = author
-            article.save()
-
         pub_form = bc_forms.PublicationInfo(request.POST, instance=article)
 
         if pub_form.is_valid():
@@ -454,12 +280,4 @@ def handleSaveForm(request, article):
 
     return True
 
-def setAuthorOrder(article,author):
-    order=article.next_author_sort() 
-    aao=submission_models.ArticleAuthorOrder()
-    aao.article=article
-    aao.author=author
-    aao.order=order
-    aao.save()
-    return aao
 
